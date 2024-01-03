e171117bbb7bb1747550045a68bd54d3aea9d824
Verner Haimbili <wernerhaimbili@gmail.com>
2024-01-03 16:07:11 +0200

    Learning

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-    message: "Hello World",
-    name: "Werner",
-    price: "1000",
-    path: "https://swimandsweat.com/cdn/shop/files/Tile-1_646451f2-e7e7-4fa8-a275-51e762891bd0.jpg?v=1685627923&width=750",
-    link: "https://www.google.com",
-  },
-  methods: {
-    discount: function (d) {
-      cost = this.price - d;
-      return cost;
+    el: "#app",
+    data: {
+        message: "Hello World",
+        name: "Werner",
+        price: "1000",
+        path: "https://swimandsweat.com/cdn/shop/files/Tile-1_646451f2-e7e7-4fa8-a275-51e762891bd0.jpg?v=1685627923&width=750",
+        link: "https://www.google.com",
+        ackn: 'Vue is nice'
     },
-  },
+    methods: {
+        discount: function(d) {
+            cost = this.price - d;
+            return cost;
+        },
+    },
 });
