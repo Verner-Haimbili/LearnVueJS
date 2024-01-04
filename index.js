d97ad5b6db73355f0b698e9c96e0ba7dd0754a39
Verner Haimbili <wernerhaimbili@gmail.com>
2024-01-04 10:55:29 +0200

    Edition done

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ var app = new Vue({
         price: "1000",
         path: "https://swimandsweat.com/cdn/shop/files/Tile-1_646451f2-e7e7-4fa8-a275-51e762891bd0.jpg?v=1685627923&width=750",
         link: "https://www.google.com",
-        ackn: 'Vue is nice'
+        ackn: "Vue is nice",
+        temp: 0,
+        dataToDisplay: "",
+        status: false,
+        gate: false,
     },
+
     methods: {
         discount: function(d) {
             cost = this.price - d;
             return cost;
+        },
+        increase: function() {
+            this.temp++;
+        },
+        decrease: function() {
+            this.temp--;
+        },
+        keypressed: function() {
+            console.log("key pressed");
         },
     },
 });
